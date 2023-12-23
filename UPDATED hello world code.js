d72f06e45c89c605d5842c1aacfb6f93d5d5b88d
vivtal <154624058+vivtal@users.noreply.github.com>
2023-12-22 20:27:17 -0800

    Add files via upload
    Okay! I was worried the other code & terminal PDF wouldn't suffice, so I redid the code for all the tasks (and learned a few more things!)

---
+++ UPDATED hello world code.js
@@ -0,0 +1,34 @@
+/*  
+Welcome, new coder. 
+Enter your code on line 7 to print 'Hello, World!' 
+to the console (the panel on the right)
+*/
+
+console.log("Hello World!");
+console.log("The code will not work without parentheses since they define the parameters, in this case, text.");
+console.log("We need to include both opening and closing quote marks, although it doesn't matter if it's single or double, as long as they match.")
+console.log("Removing the semicolon is fine.")
+console.log("Print a number: 5")
+console.log("Printing two numbers added together:", 1 + 1)
+console.log("Two messages")
+console.log("One after another")
+console.log("Two messages", "on the same line")
+console.log('Quoth the Raven "Nevermore"')
+const input = require("readline-sync");
+let name = input.question("Enter your name: ")
+console.log("Hello, " + name + "!")
+
+/*  
+Next, PLAY! 
+Modify your code on line 7 to try to accomplish the tasks listed below:
+
+   1. Change the message that is printed.
+   2. Figure out what the parentheses do. Will the code work without them?
+   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
+   4. Remove the semi-colon, ;.
+   5. Print a number. (Bonus: Print two numbers added together).
+   6. Print multiple messages one after the other.
+   7. Print two messages on the same line.
+   8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
+   9. Other. You choose!
+*/
